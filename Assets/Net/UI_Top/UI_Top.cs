c9ca6fb2b9e9796774334b7175af8bc7fa6a907e
GodHongIl <adunstudio@daum.net>
2022-08-31 22:29:07 +0900

    오아오와우어ㅏ

---
+++ Assets/Net/UI_Top/UI_Top.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Top : MonoBehaviour
+{
+    public void OnClickMenuButton()
+    {
+        
+    }
+
+    public void OnClickCatListButton()
+    {
+        
+    }
+
+    public void OnClickSettingButton()
+    {
+        
+    }
+}
