a860603a9c93c32ad54cbd82af3441092131495f
leepers97 <111097465+leepers97@users.noreply.github.com>
2022-09-01 02:17:11 +0900

    TextUI

--- Assets/Scripts/HPManager.cs
+++ Assets/Scripts/HPManager.cs
@@ -6,7 +6,11 @@ public class HPManager : MonoBehaviour
 {
     public int hp = 100;
     float currentTime;
-    public float damageTime = 1; 
+    public float damageTime = 1;
+
+    public GameObject StateGood;
+    public GameObject StateNormal;
+    public GameObject StateBad;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +24,26 @@ public class HPManager : MonoBehaviour
         if (currentTime > damageTime)
         {
             hp--;
-            currentTime = 0;
+            currentTime = 0;        
         }
-
+        if(hp >= 70)
+        {
+            StateGood.SetActive(true);
+            StateNormal.SetActive(false);
+            StateBad.SetActive(false);
+        }
+        else if (hp < 30)
+        {
+            StateGood.SetActive(false);
+            StateNormal.SetActive(false);
+            StateBad.SetActive(true);
+        }
+        else
+        {
+            StateGood.SetActive(false);
+            StateNormal.SetActive(true);
+            StateBad.SetActive(false);
+        }
         hp = Mathf.Clamp(hp, 0, 100);
     }
 }
